e933800538a6e968c6469eb3d2e79c5797b2302e
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-12-19 01:31:03 -0600

    Invoke navigation service when query is submitted

--- UserControls/NavigationBar.xaml.cs
+++ UserControls/NavigationBar.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Web;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,6 +23,25 @@ namespace Pulse_Browser.UserControls
         public NavigationBar()
         {
             this.InitializeComponent();
+        }
+
+        private void AutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            if (!string.IsNullOrEmpty(args.QueryText))
+            {
+                bool isUri = Uri.TryCreate(args.QueryText, UriKind.Absolute, out Uri destination)
+                    && (destination.Scheme == Uri.UriSchemeHttp || destination.Scheme == Uri.UriSchemeHttps);
+
+                if (isUri)
+                {
+                    Services.NavigationService.Navigation(destination);
+                }
+                else
+                {
+                    Uri searchAddress = new Uri($"https://www.google.com/search?q={HttpUtility.UrlEncode(args.QueryText)}");
+                    Services.NavigationService.Navigation(searchAddress);
+                }
+            }
         }
     }
 }
