bd866b7384d0301cbd8b5bdc7751cc20be3e513e
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-12-19 08:22:39 -0600

    Added AppContentDialog helper

---
+++ Helpers/AppContentDialog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Pulse_Browser.Helpers
+{
+    /// <summary>
+    /// A central Content Dialog helper to help manage dialogs
+    /// </summary>
+    public static class AppContentDialog
+    {
+        private static readonly ContentDialog dialog = new ContentDialog();
+
+        public static bool IsOpen = false;
+
+        public static async void OpenDialog(FrameworkElement content)
+        {
+            dialog.Content = content;
+            if (!IsOpen)
+            {
+                await dialog.ShowAsync();
+            }
+        }
+
+        public static async Task OpenDialogAsync(FrameworkElement content)
+        {
+            dialog.Content = content;
+            if (!IsOpen)
+            {
+                await dialog.ShowAsync();
+            }
+        }
+
+        public static void Hide()
+        {
+            dialog.Hide();
+        }
+    }
+}
