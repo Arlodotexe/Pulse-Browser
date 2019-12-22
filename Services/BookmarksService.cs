7e1b887afd5ec6678177f47116ce8f4427ee1797
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-12-22 10:43:19 -0600

    Added BookmarksService

---
+++ Services/BookmarksService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pulse_Browser.Services
+{
+    public class Bookmark
+    {
+        public Uri Uri { get; set; }
+        public Uri Icon { get; set; }
+    }
+
+    public static class BookmarksService
+    {
+        public static async Task<List<Bookmark>> GetBookmarks()
+        {
+            List<Bookmark> Bookmarks = (await Helpers.Storage.GetLocalClass<IEnumerable<Bookmark>>("Bookmarks"))?.ToList() ?? new List<Bookmark>();
+
+            return Bookmarks;
+        }
+
+        public static async void AddBookmark(Bookmark bookmark)
+        {
+            List<Bookmark> Bookmarks = (await Helpers.Storage.GetLocalClass<IEnumerable<Bookmark>>("Bookmarks"))?.ToList() ?? new List<Bookmark>();
+
+            Bookmarks.Add(bookmark);
+
+            await Helpers.Storage.StoreLocalClass("Bookmarks", Bookmarks);
+        }
+    }
+}
