9ceffe319584a298da47d91a7acbc7d317ddaaf8
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-12-26 14:32:25 -0600

    Implemented theme selection handlers

--- UserControls/Settings.xaml.cs
+++ UserControls/Settings.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pulse_Browser.Services;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,6 +23,56 @@ namespace Pulse_Browser.UserControls
         public Settings()
         {
             this.InitializeComponent();
+            PopulateTheme();
+        }
+
+
+        #region Theme selection handlers
+        private void SystemMode_Selected(object sender, RoutedEventArgs e)
+        {
+            SettingsService.SetLocal(SettingKeys.PrefferedTheme, ElementTheme.Default);
+            if (Window.Current.Content is FrameworkElement frameworkElement)
+            {
+                frameworkElement.RequestedTheme = ElementTheme.Default;
+            }
+        }
+
+        private void DarkMode_Selected(object sender, RoutedEventArgs e)
+        {
+            SettingsService.SetLocal(SettingKeys.PrefferedTheme, ElementTheme.Dark);
+            if (Window.Current.Content is FrameworkElement frameworkElement)
+            {
+                frameworkElement.RequestedTheme = ElementTheme.Dark;
+            }
+        }
+
+        private void LightMode_Selected(object sender, RoutedEventArgs e)
+        {
+            SettingsService.SetLocal(SettingKeys.PrefferedTheme, ElementTheme.Light);
+            if (Window.Current.Content is FrameworkElement frameworkElement)
+            {
+                frameworkElement.RequestedTheme = ElementTheme.Light;
+            }
+        }
+        #endregion
+
+        private void PopulateTheme()
+        {
+            ElementTheme PrefferedTheme = SettingsService.GetLocal<ElementTheme>(SettingKeys.PrefferedTheme);
+            switch (PrefferedTheme)
+            {
+                case ElementTheme.Default:
+                    SystemThemeRadioButton.IsChecked = true;
+                    break;
+                case ElementTheme.Dark:
+                    DarkThemeRadioButton.IsChecked = true;
+                    break;
+                case ElementTheme.Light:
+                    LightThemeRadioButton.IsChecked = true;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
