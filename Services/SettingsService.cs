098d30e1ce079b740191ca4d8eb7ce2b8a4eeea2
Arlo Godfrey <arlo.godfrey@outlook.com>
2019-12-26 14:23:04 -0600

    Added settings service

---
+++ Services/SettingsService.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+
+namespace Pulse_Browser.Services
+{
+    public enum SettingKeys
+    {
+        PrefferedTheme
+    }
+
+    public static class SettingsService
+    {
+        private static readonly IPropertySet LocalSettings = ApplicationData.Current.LocalSettings.Values;
+
+        public static T GetLocal<T>(SettingKeys key) => LocalSettings.ContainsKey(key.ToString()) ? (T)LocalSettings[key.ToString()] : default;
+
+        public static void SetLocal<T>(SettingKeys key, T value)
+        {
+            if (!LocalSettings.ContainsKey(key.ToString())) LocalSettings.Add(key.ToString(), value);
+            else LocalSettings[key.ToString()] = value;
+        }
+    }
+}
